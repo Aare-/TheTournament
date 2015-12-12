cbbc2699a70998281dcf14a3174a8693c59c733e
fblost <fblost@gmail.com>
2015-12-12 18:19:14 +0100

    Initial commit

---
+++ code/LD34/Assets/Scripts/Utils/ExtensionVector2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+static class ExtensionVector2 {
+    public static Vector2 Rotate(this Vector2 v, float degrees) {
+        float radians = degrees * Mathf.Deg2Rad;
+        float sin = Mathf.Sin(radians);
+        float cos = Mathf.Cos(radians);
+
+        float tx = v.x;
+        float ty = v.y;
+
+        return new Vector2(cos * tx - sin * ty, sin * tx + cos * ty);
+    }
+    public static Vector2 Mul(this Vector2 v, float val) {        
+        return new Vector2(v.x * val, v.y * val);
+    }
+}
+
