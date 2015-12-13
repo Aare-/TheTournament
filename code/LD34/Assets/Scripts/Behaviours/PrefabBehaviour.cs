b000758a8ceb7ebf0b81d2b0377e96b11f99a468
fblost <fblost@gmail.com>
2015-12-13 20:31:58 +0100

    prefab behaviour template

--- code/LD34/Assets/Scripts/Behaviours/PrefabBehaviour.cs
+++ code/LD34/Assets/Scripts/Behaviours/PrefabBehaviour.cs
@@ -2,7 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
-class PrefabBehaviour {
+class PrefabBehaviour : StateMachineBehaviour {
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
 
+    }
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+
+    }
+
+    public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash) {
+        base.OnStateMachineEnter(animator, stateMachinePathHash);
+
+    }
+    public override void OnStateMachineExit(Animator animator, int stateMachinePathHash) {
+        base.OnStateMachineExit(animator, stateMachinePathHash);
+
+    }
 }
