b3975671a1fa39f841dbee44527ac94f1acb8167
Kamil Spisak <Kamil Spisak>
2015-12-13 21:42:49 +0100

    Dodanie grafik i animacji do strzałek (commit razem z biblioteka iTween).

--- code/LD34/Assets/Arrow.cs
+++ code/LD34/Assets/Arrow.cs
@@ -6,25 +6,29 @@ public class Arrow : MonoBehaviour {
 
     public Sprite arrowReleased = null;
     public Sprite arrowPressed = null;
+    public Vector3 defaultPosition;
     public int id;
 
     void Start()
     {
         TinyTokenManager.Instance.Register<Msg.ArrowClicked>("ARROW_" + id + "_PRESSED", OnArrowPressed);
         TinyTokenManager.Instance.Register<Msg.ArrowReleased>("ARROW_" + id + "_RELEASED", OnArrowReleased);
+        defaultPosition = transform.position;
     }
 	
     void OnArrowPressed(Msg.ArrowClicked m)
     {
-        //GetComponent<Image>().sprite = arrowPressed;
-        if(m.ArrowID == id)
-            GetComponent<Image>().color = Color.red;
+        if (m.ArrowID == id)
+        {
+            iTween.MoveBy(gameObject, new Vector3(0, -0.1f, 0), 0.5f);
+        }
     }
 
     void OnArrowReleased(Msg.ArrowReleased m)
     {
-        //GetComponent<Image>().sprite = arrowReleased;
         if (m.ArrowID == id)
-            GetComponent<Image>().color = Color.white;
+        {
+            iTween.MoveTo(gameObject, defaultPosition, 0.5f);
+        }
     }
 }
