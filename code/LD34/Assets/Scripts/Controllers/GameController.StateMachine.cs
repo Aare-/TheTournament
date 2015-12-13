2ad6fd2fd7c62c1333a4fbddc938c6a81f7a667f
fblost <fblost@gmail.com>
2015-12-14 00:04:20 +0100

    dupa chuj

---
+++ code/LD34/Assets/Scripts/Controllers/GameController.StateMachine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public partial class GameController : Singleton<GameController> {
+    #region Triggers
+    public const string TRIGGER_SELECT = "select_performed";
+    public const string TRIGGER_REROLL = "reroll"; 
+    #endregion
+
+    [Header("State Machine")]
+    public Animator UIStateMachine;
+
+    public void EnableTrigger(string triggerName) {
+        UIStateMachine.SetTrigger(triggerName);
+    }
+}
