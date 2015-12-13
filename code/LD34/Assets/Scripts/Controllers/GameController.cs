2b6d879e0a9e7416feca6cdef8953006c99c9954
Kamil Spisak <Kamil Spisak>
2015-12-13 17:49:11 +0100

    MainMenu Mocup with Gate Animation (TODO: Buttons)

--- code/LD34/Assets/Scripts/Controllers/GameController.cs
+++ code/LD34/Assets/Scripts/Controllers/GameController.cs
@@ -1,13 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
-	
+
+    public Button arrowLeft;
+    public Button arrowRight;
+    public RectTransform rectGate;
+
 	void Start () {
 	
 	}
 		
-	void Update () {
-	
+	void Update () 
+    {
+	    if(Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            OpenHalfGate();
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            OpenHalfGate();
+        }
 	}
+
+    public void OpenGate()
+    {
+
+    }
+
+    public void OpenHalfGate()
+    {
+
+    }
 }
---
+++ code/LD34/Assets/Scripts/MenuScreenControler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuScreenControler : MonoBehaviour {
+    
+    public Button leftArrow;
+    public Button rightArrow;
+    public RectTransform rectGate;
+    public Animator anim;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            var pointer = new PointerEventData(EventSystem.current); // pointer event for Execute
+            ExecuteEvents.Execute(leftArrow.gameObject, pointer, ExecuteEvents.pointerEnterHandler);
+            Debug.Log("Dupa");
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rightArrow.Select();
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("open_all", true);
+        }
+        else
+        {
+            anim.SetBool("open_all", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("open_half", true);
+        }
+        else
+        {
+            anim.SetBool("open_half", false);
+        }
+    }
+
+}
