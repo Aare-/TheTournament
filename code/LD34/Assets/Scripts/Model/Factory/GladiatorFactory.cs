973d7484169eda8115fecba7ec259d8920182923
fblost <fblost@gmail.com>
2015-12-14 02:14:48 +0100

    szkic gladiator factory

---
+++ code/LD34/Assets/Scripts/Model/Factory/GladiatorFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract class GladiatorFactory {
+
+    protected int _PowerLevel;
+
+    public GladiatorFactory() {
+        _PowerLevel = 0;
+    }
+
+    public virtual void SetPowerLevel(int powerLevel) {
+        _PowerLevel = powerLevel;
+    }
+
+    public virtual Gladiator Generate() {
+        Gladiator gladiator = new Gladiator();
+        return gladiator;
+    }
+}
+
