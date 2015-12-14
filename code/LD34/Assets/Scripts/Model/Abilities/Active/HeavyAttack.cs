dc05d83e97bd7e288e9904ce9dbc36368c7e85a3
fblost <fblost@gmail.com>
2015-12-14 23:00:53 +0100

    wyswietlanie listy skilli na ekranie wyboru gladiatora

--- code/LD34/Assets/Scripts/Model/Abilities/Active/HeavyAttack.cs
+++ code/LD34/Assets/Scripts/Model/Abilities/Active/HeavyAttack.cs
@@ -4,33 +4,29 @@ using TinyMessenger;
 
 //Deal 10-15 damage - can be Red, Blue, Green
 public class HeavyAttack : ActiveAbility {
+    private AbilityColor _Color;
 
-	public override AbilityColor Color
-    {
-        get { return Color; }
+	public override AbilityColor Color {
+        get { return _Color; }
         set { }
     }
 
-    public override int Level
-    {
+    public override int Level {
         get { return 2; }
     }
 
-    public HeavyAttack(AbilityColor color)
-    {
-        Color = color;
+    public HeavyAttack(AbilityColor color) {
+        _Color = color;
     }
 
-    public override void ExecuteOnOpponent(Gladiator gladiator)
-    {
+    public override void ExecuteOnOpponent(Gladiator gladiator) {
         int damage = Random.Range(10, 15);
         gladiator.Life -= damage;
         TinyMessengerHub.Instance.Publish<Msg.ShowDamage>(new Msg.ShowDamage(gladiator._Id, damage));
         
     }
 
-    public override void ExecuteOnAlly(Gladiator gladiator)
-    {
+    public override void ExecuteOnAlly(Gladiator gladiator) {
         gladiator.Adrenaline -= 10;
     }
 }
--- code/LD34/Assets/Scripts/Screens/ChooseGladiator/MyGladiatorInfo.cs
+++ code/LD34/Assets/Scripts/Screens/ChooseGladiator/MyGladiatorInfo.cs
@@ -5,7 +5,12 @@ using System.Collections.Generic;
 
 public class MyGladiatorInfo : MonoBehaviour {
 
+    [Header("Links to the prefabs")]
+    public ActionDetails _ActionDetailItem;
+
+    [Header("Game Objects")]
     public GameObject AvatarContainer;
+    public GridLayoutGroup SkillsList;
     public Text name;
     public Text level;
     public Text adrenaline;
@@ -35,5 +40,22 @@ public class MyGladiatorInfo : MonoBehaviour {
         o.transform.SetParent(AvatarContainer.transform, false);
         RectTransform r = o.GetComponent<RectTransform>();
         o.transform.localPosition = new Vector3(-28, -32, 0);
+
+        SkillsList.gameObject.DeleteAllChildreen();
+        #region Loading Passive abilities list
+        foreach (PassiveAbility p in g.PassiveAbilities) {
+            ActionDetails c = (ActionDetails)Instantiate(_ActionDetailItem);
+            c.SetAbility(p);
+            c.transform.SetParent(SkillsList.transform, false);
+        }
+        #endregion
+
+        #region Loading Active abilities list
+        foreach (ActiveAbility ac in g.ActiveAbilities) {
+            ActionDetails c = (ActionDetails)Instantiate(_ActionDetailItem);
+            c.SetAbility(ac);
+            c.transform.SetParent(SkillsList.transform, false);
+        }
+        #endregion
     }
 }
