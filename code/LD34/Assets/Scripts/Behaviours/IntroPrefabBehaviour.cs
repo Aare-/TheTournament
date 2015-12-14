c1ef18570ca9813934f258fb2d0c78d9ceed1af6
fblost <fblost@gmail.com>
2015-12-14 02:54:02 +0100

    znikanie strzalek w menu

--- code/LD34/Assets/Scripts/Behaviours/IntroPrefabBehaviour.cs
+++ code/LD34/Assets/Scripts/Behaviours/IntroPrefabBehaviour.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 class IntroPrefabBehaviour : PrefabBehaviour{
     public List<string> DisableOnEnter;
+    GameObject controls;
 
     protected override void InitPrefabs() {
         base.InitPrefabs();
 
-        
+        controls = GameObject.FindGameObjectWithTag("Controls");
+        controls.SetActive(false);
     }
     protected override void DestroyPrefabs() {
         base.DestroyPrefabs();
-
+        controls.SetActive(true);
     }
 
     /*virtual void InitPrefabs() {
