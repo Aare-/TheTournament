cc84cb4d832662d5a0d914311e72036d54fa0edf
fblost <fblost@gmail.com>
2015-12-15 18:20:40 +0100

    fejd w menu glownym

--- code/LD34/Assets/Scripts/Screens/MainMenu/MenuScreenControler.cs
+++ code/LD34/Assets/Scripts/Screens/MainMenu/MenuScreenControler.cs
@@ -8,16 +8,35 @@ public class MenuScreenControler : MonoBehaviour {
     
     public RectTransform rectGate;
     public GameObject stamp;
+    public Image BlackFade;
     public Animator anim;
 
+    public float FadeInTime = 1.0f;
+    float _FadeInTime = 1.0f;
+
+    void OnAwake() {
+        BlackFade.gameObject.SetActive(true);
+        BlackFade.color = new Color(0, 0, 0, 1.0f);
+    }
     public void OnStart() {
         GameController.Instance.player = null;
+        _FadeInTime = FadeInTime;
     }
 
     public void OnDestroy() {        
     }
 
-    void Update(){        
+    void Update(){
+
+        if (_FadeInTime > 0) {
+            if (!BlackFade.IsActive())
+                BlackFade.gameObject.SetActive(true);
+            BlackFade.color = new Color(0, 0, 0, _FadeInTime / FadeInTime);
+            _FadeInTime -= Time.deltaTime;
+        } else {
+            if(BlackFade.IsActive())
+                BlackFade.gameObject.SetActive(false);
+        }
 
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) {
             anim.SetBool("open_all", true);
